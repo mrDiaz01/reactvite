c8f9334dbba531f0a11153215e553a61179f6283
Gonçalo Dias <93800919+mrDiaz01@users.noreply.github.com>
2023-12-21 18:34:31 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Button from "./components/Button";
-import Alert from "./components/alert";
+import Alert from "./components/Alert";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
